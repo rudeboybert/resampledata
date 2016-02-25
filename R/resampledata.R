bd53750a29315653c9c776bbdfe94f2b22466839
rudeboybert <albert.ys.kim@gmail.com>
2016-02-24 19:15:06 -0500

    Edited DESCRIPTION and added .travis.yml

---
+++ R/resampledata.R
@@ -0,0 +1,12 @@
+#' resampledata: Datasets for Mathematical Statistics with Resampling in R
+#'
+#' Package to load datasets from "Datasets for Mathematical Statistics
+#' with Resampling in R" (2011) by Laura Chihara and Tim Hesterberg.
+#'
+#' @section Datasets
+#' The foo functions ...
+#'
+#' @docType package
+#' @name resampledata
+NULL
+#> NULL
