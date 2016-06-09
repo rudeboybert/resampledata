1c9648b32cd0eef2c3541847b7d219f90f189e07
rudeboybert <albert.ys.kim@gmail.com>
2016-06-09 13:20:42 +0200

    Updated documentation.

--- R/resampledata.R
+++ R/resampledata.R
@@ -1,10 +1,13 @@
 #' resampledata: Datasets for Mathematical Statistics with Resampling in R
 #'
-#' Package to load datasets from "Datasets for Mathematical Statistics
+#' Package of datasets from "Datasets for Mathematical Statistics
 #' with Resampling in R" (2011) by Laura Chihara and Tim Hesterberg.
 #'
 #' @section Datasets:
-#' Type \code{data(package="resampledata")} to view all datasets.
+#' Type \code{data(package="resampledata")} to view all datasets. Note the
+#' following datasets were not included due to sharing restrictions and can only
+#'  be accessed on the textbook's webpage: \code{Bangladesh}, \code{GSS2002},
+#'  \code{GSS2006}, and \code{Volleyball2009}.
 #'
 #' @docType package
 #' @name resampledata
