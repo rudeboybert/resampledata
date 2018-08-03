40c0b668a0d46f9cc4b4c88650bde8fc958971a4
rudeboybert <albert.ys.kim@gmail.com>
2018-08-03 18:37:13 -0400

    Updated all references to book to 2nd edition

--- R/resampledata.R
+++ R/resampledata.R
@@ -1,7 +1,7 @@
 #' resampledata: Data Sets for Mathematical Statistics with Resampling in R
 #'
 #' Package of data sets from "Mathematical Statistics with Resampling in R"
-#' (2011) by Laura Chihara and Tim Hesterberg. Note the \code{\link{Bangladesh}}
+#' (1st Ed. 2011, 2nd Ed. 2018) by Laura Chihara and Tim Hesterberg. Note the \code{\link{Bangladesh}}
 #' data is provided solely for illustrative purposes and to enable statistical
 #' analysis. Full data are available from the British Geological Survey web site
 #' \url{http://www.bgs.ac.uk/arsenic/bphase2/datadownload.htm}.
@@ -16,7 +16,7 @@
 #' @references Hesterberg, Tim (2014), What Teachers Should Know about the
 #' Bootstrap: Resampling in the Undergraduate Statistics Curriculum,
 #' \url{http://arxiv.org/abs/1411.5279}.
-#' @references Chihara, Laura M., Hesterberg, Tim C. (2011). Mathematical
+#' @references Chihara, Laura M., Hesterberg, Tim C. (1st Ed. 2011, 2nd Ed. 2018). Mathematical
 #'   Statistics with Resampling and R. Hoboken, NJ: John Wiley & Sons.
 #' @name resampledata
 NULL
